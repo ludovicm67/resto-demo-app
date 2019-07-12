76d563183830a8454a622a0b6a683b3fc9244b10
ludovicm67 <ludovicmuller1@gmail.com>
2019-07-12 16:33:27 +0200

    started to integrate redis to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 // import all required stuff
 const express = require('express');
-const server = express();
 const prom = require('prom-client');
+const redis = require('redis');
+const server = express();
 const register = prom.register;
+
+// connect to redis
+const redis_host = process.env.REDIS_HOST || 'localhost';
+const redis_port = process.env.REDIS_PORT || 6379;
+const redis_client = redis.createClient(redis_port, redis_host);
+const redis_close_connection = () => {
+ redis_client.quit();
+ console.log('connection to redis client closed.');
+};
+redis_client.on('connect', () => {
+  console.log('connected to redis.');
+});
+redis_client.on('error', (err) => {
+  console.error(`Error: unable to connect to redis (${err}).`);
+});
 
 // setup our metric
 const Gauge = prom.Gauge;
@@ -12,6 +28,7 @@ const g = new Gauge({
 	labelNames: ['worker']
 });
 
+// some random stuff
 const random_queue_size = () => {
   return Math.round(Math.random() * 50);
 }
@@ -43,3 +60,6 @@ server.get('/metrics', (_req, res) => {
 server.listen(3000, () => {
   console.log('server listening on port 3000.');
 });
+
+// close redis connection on exit
+process.on('exit', redis_close_connection);
