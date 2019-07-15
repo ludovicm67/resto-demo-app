2b5910a28956f538262b0dda5bd975e7e85b77ec
ludovicm67 <ludovicmuller1@gmail.com>
2019-07-15 10:13:38 +0200

    worker: better logs

--- worker/index.js
+++ worker/index.js
@@ -1,3 +1,21 @@
+const worker_duration = process.env.WORKER_DURATION || 10;
+const worker_name = process.env.WORKER_NAME || 'anonymous';
+const allowed_workers = [
+  'dish',
+  'drink',
+  'dessert'
+];
+
+// check if worker is allowed
+if (!allowed_workers.includes(worker_name)) {
+  console.error(`worker '${worker_name}' not allowed`);
+  process.exit(1);
+}
+
+// just say "hi"
+console.log(`worker '${worker_name}' is ready!`);
+
+// the worker is working periodically
 setInterval(() => {
-  console.log(process.env);
-}, 2000);
+  console.log(`worker '${worker_name}' is working for ${worker_duration}s...`);
+}, worker_duration * 1000);
