c69ce7be92aaf19d0c141ef66f75866f0c5d8b69
ludovicm67 <ludovicmuller1@gmail.com>
2019-07-15 10:24:06 +0200

    worker: connect to redis

--- worker/index.js
+++ worker/index.js
@@ -1,3 +1,23 @@
+// redis
+const redis = require('redis');
+const redis_host = process.env.REDIS_HOST || 'localhost';
+const redis_port = process.env.REDIS_PORT || 6379;
+const redis_client = redis.createClient(redis_port, redis_host);
+const redis_close_connection = () => {
+ redis_client.quit();
+ console.log('connection to redis client closed.');
+ process.exit(1);
+};
+redis_client.on('connect', () => {
+  console.log('connected to redis.');
+});
+redis_client.on('error', (err) => {
+  console.error(`Error: unable to connect to redis (${err}).`);
+  process.exit(1);
+});
+process.on('exit', redis_close_connection);
+
+// setup worker
 const worker_duration = process.env.WORKER_DURATION || 10;
 const worker_name = process.env.WORKER_NAME || 'anonymous';
 const allowed_workers = [
