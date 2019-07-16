efeb0d540dd48898425ea8e80cb0a0f9cee852ff
ludovicm67 <ludovicmuller1@gmail.com>
2019-07-16 16:02:57 +0200

    front: move all nginx configuration in the same place

--- front/init.sh
+++ front/init.sh
@@ -4,5 +4,8 @@
 sed -i "s~BACKEND_URL~$BACKEND_URL~g" /usr/share/nginx/html/static/js/*.js
 
 # configure and start nginx
+sed -i \
+  '/index.htm/a try_files $uri $uri/ /index.html =404;' \
+  /etc/nginx/conf.d/default.conf
 echo "daemon off;" >> /etc/nginx/nginx.conf
 nginx
